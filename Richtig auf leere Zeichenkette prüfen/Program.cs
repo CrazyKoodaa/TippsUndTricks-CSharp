df1705fb12c7eb080d7381c65e7456cb9c81d311
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 16:23:32 +0100

    Strings richtig auf Null, Empty oder Leerzeichen checken

---
+++ Richtig auf leere Zeichenkette prüfen/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace Richtig_auf_leere_Zeichenkette_prüfen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugger.Break();
+
+            // um eine Variable als string zu bezeichnen, dass geht nur mit dem @ davor
+            string @string = string.Empty; // => ""
+
+            // Null oder String.Empty
+            if (@string is null || @string == string.Empty)
+                Console.WriteLine("Null oder String.Empty");
+
+            if (string.IsNullOrEmpty(@string))
+                Console.WriteLine("Null oder String.Empty mit IsNullOrEmpty()");
+
+            // Null, String.Empty oder nur Leerzeichen abfrage
+            if (@string is null || @string == string.Empty || @string.Where(w => w == ' ').Count() == @string.Length)
+                Console.WriteLine("Null, String.Empty oder nur Leerzeichen");
+
+            if (string.IsNullOrWhiteSpace(@string))
+                Console.WriteLine("Null, String.Empty oder nur Leerzeichen");
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
