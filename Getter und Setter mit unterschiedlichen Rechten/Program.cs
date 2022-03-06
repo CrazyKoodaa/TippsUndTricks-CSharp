4e15a466ec2bcaf6379fa71d23bdeed50238bd80
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 17:15:25 +0100

    Getter und Setter mit unterschiedlichen Rechten

---
+++ Getter und Setter mit unterschiedlichen Rechten/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Getter_und_Setter_mit_unterschiedlichen_Rechten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instanceA = new ClassA("Thorsten Kansy", 4711);
+
+            string name = instanceA.Name;
+            //instanceA.Name = "Hallo";
+
+            // Ab C# 9.0
+            var instanceB = new ClassB()
+            {
+                Name = "Thorsten Kansy",
+                Code = 4711
+
+            };
+
+
+        }
+
+        public class ClassA
+        {
+            // mit dem private set, kann man nur innerhalb der Klasse die Variable beschreiben. Jedoch nicht außerhalb
+            public string Name { get; private set; }
+            public int Code { get; private set; }
+            public ClassA(string Name, int Code)
+            {
+                this.Name = Name;
+                this.Code = Code;
+            }
+
+
+        }
+
+        // Ab C# 9.0
+        public class ClassB
+        {
+            public string Name { get; init; }
+            public int Code { get; init; }
+        }
+
+    }
+}
