6f8879823a4e11decda26c0f18bdee2ef3ef65ad
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 16:09:30 +0100

    Fun mit Preprocessor Directives

---
+++ PreprocessorDirectives/Program.cs
@@ -0,0 +1,45 @@
+﻿// Man kann hier bereits eine Directive erstellen
+// Je nachdem ob man die nächste Zeile kommentiert oder nicht, sieht man im VS 2022, dass entweder das eine Ausgeführt wird oder das andere (das bunte wird ausgeführt)
+#define SPECIAL
+
+using System;
+
+
+
+namespace PreprocessorDirectives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+#pragma warning disable CS0168 // Variable is declared but never used
+            int i;
+#pragma warning restore CS0168 // Variable is declared but never used
+
+#if SPECIAL
+            Console.WriteLine("Ist Spezial");
+#else
+            Console.WriteLine("Kein SPECIAL");
+#endif
+
+#if DEBUG
+            Console.WriteLine("DEBUG Modus an");
+#endif
+
+            // Zeile vor dem Debugger verstecken:
+            Console.WriteLine("WriteLine #1");
+#line hidden
+            Console.WriteLine("WriteLine #2");
+#line default
+            Console.WriteLine("WriteLine #3");
+
+#error Refactoring angebracht!!!
+
+#warning Man sollte Refactoren
+
+
+        }
+    }
+}
