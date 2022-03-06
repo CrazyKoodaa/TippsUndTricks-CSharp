2f37d126fb5bf25068fc0827f2a513d924215323
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 16:55:39 +0100

    Erweiterungsmethoden

---
+++ Erweiterungsmethoden nutzen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Erweiterungsmethoden_nutzen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CONTENT = "Hat der alte Hexenmeister";
+            
+            //Debugger.Break();
+
+            // Aus...
+            int c = CONTENT.CountLetters(true);
+            // ..macht der Compiler
+            // c = MyFunctionLibrary.CountLetters(CONTENT);
+
+            Console.WriteLine($"Letter true = {CONTENT.CountLetters(true)}");
+            Console.WriteLine($"Letter false = {CONTENT.CountLetters(false)}");
+            Console.WriteLine($"Letter  = {CONTENT.CountLetters()}");
+        }
+    }
+
+    public static class MyFunctioLibrary
+    {
+        public static int CountLetters(this string String, bool DistinctOnly = false)
+        {
+            // Alle Zeichen ermiiteln
+            var query = String.Where(w => char.IsLetter(w));
+
+            // Nur Eindeutige?
+            if (DistinctOnly)
+                query = query.Distinct();   
+
+            // Anzahl liefern
+            return query.Count();
+
+        }
+    }
+}
