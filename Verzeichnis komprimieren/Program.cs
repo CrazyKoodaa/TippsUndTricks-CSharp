ec313e2e782c7be0caf26143c0ec3616e9fe56b0
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 18:08:03 +0100

    Verzeichnisse Komprimieren und Extrahieren

---
+++ Verzeichnis komprimieren/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Verzeichnis_komprimieren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string startPath = @".";
+            const string zipPath = @"..\BinFolder.zip";
+            const string extractPath = @"Extracted\";
+
+            // Kompletten Bin Ordner (genauer, den Ordner in dem sich die Binaries befinden) komprimieren
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            // Zip Archiv Entpacken
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+
+
+        }
+    }
+}
