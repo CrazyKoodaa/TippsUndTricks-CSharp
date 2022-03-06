40ab7babf5800052ecaaaa6d15a1ed3f62c2f2dd
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 15:05:37 +0100

    Was ist nameof

---
+++ NameOf-Operator anwenden/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NameOf_Operator_anwenden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugger.Break();
+
+            int variabel1;
+
+            Console.WriteLine(nameof(Main));
+            Console.WriteLine(nameof(variabel1));
+            Console.WriteLine(nameof(Program));
+
+            methode(null);
+        }
+
+        private static void methode(string parameter)
+        {
+            if (parameter is null)
+                throw new ArgumentNullException(nameof(parameter));
+        }
+    }
+}
