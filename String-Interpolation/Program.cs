4220c7009c0029cb5877199c36ef981acb48a7a0
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 15:10:59 +0100

    String -> $ <- Interpolation

---
+++ String-Interpolation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugger.Break();
+
+            Console.WriteLine($"Heute ist: {DateTime.Today:dd.MM.yyyy}");
+
+            string date = $"{DateTime.Now:yyyy-MM-ddTHH\\:mm\\ss.fffffffzzz}";
+            Console.WriteLine($"{nameof(date)}={date}");
+
+            long fileSize = 2346273462387;
+            Console.WriteLine($"{nameof(fileSize)}={fileSize:N0} bytes");
+
+        }
+    }
+}
