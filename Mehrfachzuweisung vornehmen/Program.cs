8a692b56e0eb2536d9dd2fdae867e8b780713b3c
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-06 16:26:56 +0100

    mögliche mehrfache Zuweisung

---
+++ Mehrfachzuweisung vornehmen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Mehrfachzuweisung_vornehmen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Debugger.Break();
+
+            // Passender Type, mehrfache Zuweisung möglich
+            int x, y, z;
+
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+            x = y = z = 0;
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+
+            // dashier geht nicht
+            //string s;
+            //ClassA c;
+
+            //s = c = null;
+
+        }
+    }
+}
